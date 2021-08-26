368041a8a227911cac389b22da8f0b6178561cbd
codecaiine <akabrouyannickn@gmail.com>
2021-08-26 15:54:57 +0000

    Add html css and Javascript

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import updateStaus from './status.js';
 
 const todoList = [{
   description: 'Go to the market',
@@ -16,15 +17,20 @@ const todoList = [{
   index: 1,
 },
 ];
-
 todoList.sort((a, b) => a.index - b.index);
 
-const listDiv = document.querySelector('.todoList');
+// Set up my local storage
+const myLocalStorage = (items) => {
+  localStorage.setItem('myList', JSON.stringify(items));
+};
+const getTasksFromLocalStorage = () => JSON.parse(localStorage.getItem('myList'));
 
-const showTasks = () => {
+const listDiv = document.querySelector('.todoList');
+const showList = () => {
+  const tList = getTasksFromLocalStorage();
   listDiv.innerHTML = '';
-  for (let i = 0; i < todoList.length; i += 1) {
-    const task = todoList[i];
+  for (let i = 0; i < tList.length; i += 1) {
+    const task = tList[i];
     const list = ` <li class="todo" id="${task.index}">
       <input type="checkbox" class="check" id="list-checkbox" name="list-checkbox">
       ${task.description}<span class="icon"><i class="fa fa-ellipsis-v"></i></span> <span class="btn-del"><i class="fa fa-trash"></i></span>
@@ -32,6 +38,31 @@ const showTasks = () => {
 
     listDiv.innerHTML += list;
   }
+
+  const check = document.querySelectorAll('.check');
+  for (let j = 0; j < check.length; j += 1) {
+    check[j].addEventListener('change', (e) => {
+      if (check[j].checked) {
+        check[j].completed = true;
+        updateStaus(e.target, tList[j]);
+        e.target.parentNode.classList.toggle('checked');
+        myLocalStorage(tList);
+      } else {
+        check[j].completed = false;
+        updateStaus(e.target, tList[j]);
+        e.target.parentNode.classList.remove('checked');
+        myLocalStorage(tList);
+      }
+    });
+  }
 };
 
-window.onload = showTasks;
+// Get Tasks From Local Storage
+window.onload = () => {
+  const getTodo = getTasksFromLocalStorage();
+
+  if (getTodo === null) {
+    myLocalStorage(todoList);
+  }
+  showList();
+};
